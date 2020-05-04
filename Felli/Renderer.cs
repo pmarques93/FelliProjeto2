6c27704870f40498afb84e3f39879a0c1ad92aee
Pedro Marques <pmarques93@gmail.com>
2020-05-04 20:46:53 +0100

    Fix - Text messages

--- Felli/Renderer.cs
+++ Felli/Renderer.cs
@@ -115,12 +115,12 @@ namespace Felli
                 case "MovementString":
                     Console.WriteLine("\n -------------------- INVALID INPUT" +
                     " ---------------------\n"+
-                    "Please, insert a number, not a string.\n");
+                    " Please, insert a number, not a string.\n");
                     break;
                 case "MovementTooBig":
                     Console.WriteLine("\n -------------------- INVALID INPUT" +
                     " ---------------------\n"+
-                    "Please, insert a valid number.\n");
+                    " Please, insert a valid number.\n");
                     break;
                 case "NoPlaysP1":
                     Console.WriteLine("\n -----------------------------" +
@@ -187,22 +187,22 @@ namespace Felli
 
         public void PrintRules()
         {
-            Console.WriteLine("--------------------- FELLI GAME --------------");
-            Console.WriteLine("----------------------- Rules ------------------");
-            Console.WriteLine("- 6 White pieces are positioned on the bottom.");
-            Console.WriteLine("- 6 Black pieces are positioned on the top.");
-            Console.WriteLine("- Each player gets a turn.");
-            Console.WriteLine("- On its turn, the player may move one piece," +
+            Console.WriteLine(" --------------------- FELLI GAME --------------");
+            Console.WriteLine(" ----------------------- Rules ------------------");
+            Console.WriteLine(" - 6 White pieces are positioned on the bottom.");
+            Console.WriteLine(" - 6 Black pieces are positioned on the top.");
+            Console.WriteLine(" - Each player gets a turn.");
+            Console.WriteLine(" - On its turn, the player may move one piece," +
             " a house per turn.");
-            Console.WriteLine("- Pieces may be moved in every direction if theres" +  
+            Console.WriteLine(" - Pieces may be moved in every direction if theres" +  
             "an empty space next to it.");
-            Console.WriteLine("- Pieces may also move, jumping over the opponent's" +
+            Console.WriteLine(" - Pieces may also move, jumping over the opponent's" +
             ", eliminating it \n  from the game and landing on the house adjacent" +  
             " in that direction");
-            Console.WriteLine("- Only a single piece may be eliminated each turn.");
-            Console.WriteLine("- The game ends when a player eliminates all" + 
+            Console.WriteLine(" - Only a single piece may be eliminated each turn.");
+            Console.WriteLine(" - The game ends when a player eliminates all" + 
             " the opponents pieces.");
-            Console.WriteLine("- White pieces go first.");
+            Console.WriteLine(" - White pieces go first.");
          
         }
         private void PossiblePick(Player[] playerOne, Player[] playerTwo, 
